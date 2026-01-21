4d42e584f5669aa3fc2586a874255f7e2e5e2839
Caleb Hu <calebhu552@gmail.com>
2026-01-21 13:40:42 -0800

    updated next config for github deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const isProd = process.env.NODE_ENV === "production";
+const repoPath = "/portfolio"; // replace with your repo name
+export default {
+  output: "export",
+  ...(isProd && {
+    basePath: repoPath,
+    assetPrefix: repoPath,
+  }),
+};
